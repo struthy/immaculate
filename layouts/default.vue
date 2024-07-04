4c9c5bccd30cb4b7a28ce9b9d01e6eafb3b7ea04
Adam Struth <adam.struth@iesve.com>
2024-07-04 13:56:24 +0100

    initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const route = useRoute()
+</script>
+
+<template>
+  <div class="text-slate-700">
+   
+    <Header
+      :with-profile="route.meta.withHeaderProfile"
+      :with-divider="route.meta.withHeaderDivider"
+    />
+    <slot />
+    <Footer :with-sign-up-form="route.meta.withFooterSignUpForm" />
+  </div>
+</template>
---
+++ slices/ImageTextBlock/index.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { type Content } from "@prismicio/client";
+
+// The array passed to `getSliceComponentProps` is purely optional.
+// Consider it as a visual hint for you when templating your slice.
+defineProps(
+  getSliceComponentProps<Content.ImageTextBlockSlice>([
+    "slice",
+    "index",
+    "slices",
+    "context",
+  ]),
+);
+</script>
+
+<template>
+  <Bounded
+    as="section"
+  >
+    <figure class="grid grid-cols-12 gap-4">
+      <div
+        v-if="slice.primary.image.url"
+        class="bg-gray-100 col-span-4"
+      >
+        <PrismicImage
+          :field="slice.primary.image"
+          :width="slice.primary.image.dimensions.width"
+          :height="slice.primary.image.dimensions.height"
+        />
+      </div>
+      <div class="col-span-8">
+        <PrismicRichText
+          v-if="$prismic.asText(slice.primary.text)"
+          :field="slice.primary.text"
+          wrapper="figcaption"
+          class="text-left text-base"
+        />
+    </div>
+    </figure>
+  </Bounded>
+</template>
