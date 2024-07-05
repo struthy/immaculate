cd35058d21a6f21215e5f70b851169d6c3e360af
Your Name <you@example.com>
2024-07-05 11:03:49 +0000

    Updates

---
+++ _nuxt/BVwXLYRJ.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DZItloal.js","./BAecDz0t.js","./g4ZrY-mY.js","./CpxxLPKQ.js","./Ds41-09C.js","./BO8Rmbyr.js","./VGr_Ph-o.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{C as _,D as t,E as o}from"./BAecDz0t.js";const r=_({contact_form:t(()=>o(()=>import("./DZItloal.js"),__vite__mapDeps([0,1,2]),import.meta.url)),image:t(()=>o(()=>import("./CpxxLPKQ.js"),__vite__mapDeps([3,1,2]),import.meta.url)),image_text_block:t(()=>o(()=>import("./Ds41-09C.js"),__vite__mapDeps([4,1,2]),import.meta.url)),quote:t(()=>o(()=>import("./BO8Rmbyr.js"),__vite__mapDeps([5,2,1]),import.meta.url)),text:t(()=>o(()=>import("./VGr_Ph-o.js"),__vite__mapDeps([6,1,2]),import.meta.url))});export{r as c};
