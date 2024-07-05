05e9060e35160b550e703c316315b55054f026b2
Your Name <you@example.com>
2024-07-05 11:10:20 +0000

    Updates

--- assets/GSSzKAw2.js
+++
@@ -1,7 +0,0 @@
-function __vite__mapDeps(indexes) {
-  if (!__vite__mapDeps.viteFileDeps) {
-    __vite__mapDeps.viteFileDeps = ["./CcZC8qFw.js","./MBHCBpWb.js","./BRP4ADVc.js","./B-vAj9RU.js","./B5zreG6O.js","./8_Bcbc5g.js","./BzLCMloy.js"]
-  }
-  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
-}
-import{C as _,D as t,E as o}from"./MBHCBpWb.js";const r=_({contact_form:t(()=>o(()=>import("./CcZC8qFw.js"),__vite__mapDeps([0,1,2]),import.meta.url)),image:t(()=>o(()=>import("./B-vAj9RU.js"),__vite__mapDeps([3,1,2]),import.meta.url)),image_text_block:t(()=>o(()=>import("./B5zreG6O.js"),__vite__mapDeps([4,1,2]),import.meta.url)),quote:t(()=>o(()=>import("./8_Bcbc5g.js"),__vite__mapDeps([5,2,1]),import.meta.url)),text:t(()=>o(()=>import("./BzLCMloy.js"),__vite__mapDeps([6,1,2]),import.meta.url))});export{r as c};
